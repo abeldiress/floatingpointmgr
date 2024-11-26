bf9d70050ccf46c67f0ecd015322b59b9325d0a9
Abel Diress <abeldiress05@gmail.com>
2024-11-26 20:02:53 +0000

    added float-fixed benchmark

--- benchmarks/benchmark_FloatingPointManager.cpp
+++ benchmarks/benchmark_FloatingPointManager.cpp
@@ -1,14 +1,27 @@
 #include "../include/FloatingPointManager.h"
 #include <benchmark/benchmark.h>
 
-static void BM_BFloat16Conversion(benchmark::State& state) {
-  FloatingPointManager manager;
+static void BM_BFloat16Conversion(benchmark::State &state) {
+  FloatingPointManager mgr;
   float value = 1.23456;
 
   for (auto _ : state) {
-    uint16_t bfloat16 = manager.FloatToBFloat16(value);
-    benchmark::DoNotOptimize(manager.BFloat16ToFloat(bfloat16));
+    uint16_t bfloat16 = mgr.FloatToBFloat16(value);
+    benchmark::DoNotOptimize(mgr.BFloat16ToFloat(bfloat16));
   }
 }
+
+static void BM_FloatToFixedPoint(benchmark::State &state) {
+  FloatingPointManager mgr;
+  float value = 1.2346;
+  int fractional = 4;
+
+  for (auto _ : state) {
+    int32_t fixed_point = mgr.FloatToFixedPoint(value, fractional);
+    benchmark::DoNotOptimize(mgr.FixedPointToFloat(fixed_point, fractional));
+  }
+}
+
 BENCHMARK(BM_BFloat16Conversion);
+BENCHMARK(BM_FloatToFixedPoint);
 BENCHMARK_MAIN();
--- main.cpp
+++ main.cpp
@@ -6,14 +6,16 @@ using namespace std;
 int main() {
   // PI test
   FloatingPointManager mgr;
-  float value = 3.141592;
+  float value = 3.14159f;
 
   uint16_t bfloat16 = mgr.FloatToBFloat16(value);
   float recovered = mgr.BFloat16ToFloat(bfloat16);
+  float error = mgr.ComputeError(value, recovered);
 
   cout << "original: " << value << endl;
   cout << "bfloat16: " << bfloat16 << endl;
   cout << "recovered: " << recovered << endl;
+  cout << "error: " << error << endl;
 
   return 0;
 }
